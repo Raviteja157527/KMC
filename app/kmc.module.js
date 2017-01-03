01c07e56d594486dc6924be0c9ef86b80166a327
Raviteja536741 <pullariraviteja@gmail.com>
2017-01-03 16:39:14 +0000

    Update kmc.module.js

--- app/kmc.module.js
+++ app/kmc.module.js
@@ -9,6 +9,18 @@
 		.state('home',{
 			url:'/home',
 			templateUrl:'app/modules/home/home.html'
+		}).state('doctors-staff',{
+			url:'/doctors-staff',
+			templateUrl:'app/modules/home/doctors-staff.html'
+		}).state('surgery-times',{
+			url:'/surgery-times',
+			templateUrl:'app/modules/home/surgery-times.html'
+		}).state('clinics',{
+			url:'/clinics',
+			templateUrl:'app/modules/home/clinics.html'
+		}).state('contact-us',{
+			url:'/contact-us',
+			templateUrl:'app/modules/home/contact-us.html'
 		});
 		$locationProvider.html5Mode(true);
 	}])
